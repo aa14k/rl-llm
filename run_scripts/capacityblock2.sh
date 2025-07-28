2c703ba13882c890ae13293361909ec3eb8b378f
Ubuntu <ubuntu@ip-172-31-11-231.us-east-2.compute.internal>
2025-07-28 22:37:51 +0000

    minor fixes

--- run_scripts/capacityblock2.sh
+++ run_scripts/capacityblock2.sh
@@ -2,7 +2,7 @@ python grpo_discounted.py \
     --model_name "Qwen/Qwen2.5-7B-Instruct" \
     --gamma 1.0 \
     --seed 44 \
-    --machine_name "constantlr-1epoch-capacityblock2"
+    --machine_name "constantlr-1epoch-capacityblock2"\
     --sync_ref_model \
     --disable_dropout \
-    --sync_ref_model_steps 16 \
+    --ref_model_sync_steps 16 \
