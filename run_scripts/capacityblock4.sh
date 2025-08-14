e4940f1b134dbf2c933bc8182677f95882f78b63
Ubuntu <ubuntu@ip-172-31-14-91.us-west-2.compute.internal>
2025-08-14 00:15:07 +0000

    math500 trainer

--- run_scripts/capacityblock4.sh
+++ run_scripts/capacityblock4.sh
@@ -2,8 +2,7 @@ accelerate launch --config_file configs/zero3.yaml train_grpo_discounted_math.py
     --model_name "Qwen/Qwen2.5-7B-Instruct" \
     --gamma 0.9999999 \
     --seed 42 \
-    --machine_name "math-182" \
+    --machine_name "math-228" \
     --disable_dropout \
     --num_generations 4 \
     --gradient_accumulation_steps 4 \
-    --beta 0.0  
